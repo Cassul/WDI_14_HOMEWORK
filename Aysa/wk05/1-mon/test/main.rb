139d62bca355a2ea167a95492ae446e9348e08ee
Cassul <aysa.garden@gmail.com>
2018-01-10 11:12:31 +1100

    homework Tuesday

--- Aysa/wk05/1-mon/test/main.rb
+++ Aysa/wk05/1-mon/test/main.rb
@@ -1,4 +1,3 @@
-    
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
@@ -8,11 +7,37 @@ get '/' do
   erb :search
 end
 
+
+class OMDB
+  def query (query_type, search_for)
+    enc_uri = URI.escape("http://www.omdbapi.com/?#{query_type}=#{search_for}&apikey=2f6435d9") #encode to URL
+    result = HTTParty.get(enc_uri)
+    return result.parsed_response
+  end
+end
+omdb = OMDB.new
+
 get '/search' do
-  url = params['title']
-  enc_uri = URI.escape("http://www.omdbapi.com/?t=#{url}&apikey=2f6435d9") #encode to URL
-  result = HTTParty.get(enc_uri)
-  hash = result.parsed_response
+  hash = omdb.query('s', params[:title])
+  @titles = []
+  hash["Search"].each do |title| 
+    @titles.push(title["Title"])
+  end
+  if @titles.length > 1
+    erb :search_result
+  else 
+    hash = omdb.query('t', params[:title])
+    @title = hash["Title"]
+    @plot = hash["Plot"]
+    @rate = hash["Rate"]
+    @year = hash["Year"]
+    @image_src = hash["Poster"]
+    erb :result
+  end
+end
+
+get '/movie' do
+  hash = omdb.query('t', params[:title])
   @title = hash["Title"]
   @plot = hash["Plot"]
   @rate = hash["Rate"]
@@ -20,4 +45,3 @@ get '/search' do
   @image_src = hash["Poster"]
   erb :result
 end
-
