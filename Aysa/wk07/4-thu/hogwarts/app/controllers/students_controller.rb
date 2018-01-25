e6a83b2d98b1305c255e55a7a334ce93c58c4a24
Cassul <aysa.garden@gmail.com>
2018-01-25 20:15:18 +1100

    hogwarts assignment

---
+++ Aysa/wk07/4-thu/hogwarts/app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+    @houses = House.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    @house = House.find(@student.house_id)
+
+  end
+
+end
