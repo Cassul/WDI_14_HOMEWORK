2fe2856f8e987a05990943ce9aeee3571fd016f0
Cassul <aysa.garden@gmail.com>
2018-02-15 09:10:12 +1100

    Wednesday homework

---
+++ Aysa/wk10/3-wed/dataTypes.js
@@ -0,0 +1,52 @@
+console.log(typeof 15)
+// Prediction: number
+// Actual:
+
+console.log(typeof 5.5)
+// Prediction: number
+// Actual:
+
+console.log(typeof NaN)
+// Prediction: NaN
+// Actual:
+
+console.log(typeof "hello")
+// Prediction: string
+// Actual:
+
+console.log(typeof true)
+// Prediction: boolean
+// Actual:
+
+console.log(typeof 1 != 2) 
+// Prediction: boolean
+// Actual:
+
+
+console.log("hamburger" + "s")
+// Prediction: hamburgers
+// Actual:
+
+console.log("hamburgers" - "s")
+// Prediction: error
+// Actual:
+
+console.log("1" + "3")
+// Prediction: "13"
+// Actual:
+
+console.log("1" - "3")
+// Prediction: -2
+// Actual:
+
+console.log("johnny" + 5)
+// Prediction: "johnny5"
+// Actual:
+
+console.log("johnny" - 5)
+// Prediction: error
+// Actual:
+
+console.log(99 * "luftbaloons")
+// Prediction: error
+// Actual:
---
+++ Aysa/wk10/3-wed/jsMethods.js
@@ -0,0 +1,17 @@
+// Arrays
+// Javascript provides us with a number of native methods that allow us to interact with arrays. Find methods that do each of the following and provide an example...
+
+// Add an element to the back of an array.
+
+[1, 2, 3].push(1) => [1, 2, 3, 1]
+
+// Remove an element from the back of an array.
+[1, 2, 3].pop() => [1, 2]
+// Add an element to the front of an array.
+[1, 2, 3].shift(1) => [1, 1, 2, 3]
+// Remove an element from the front of an array.
+[1, 2, 3].unshift() => [2, 3]
+// Concatenates all the elements in an array into a string.
+[1, 2, 3].join("") => "123"
+// Separates the characters of a string into an array. This one is a string method.
+"string".split() ==> ["s", "t", "r","i","n","g"]
